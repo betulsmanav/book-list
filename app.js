62cb337f54f3f50098ae334e573271059eb3fa48
betul <betulsmanav@gmail.com>
2022-02-27 10:17:45 +0300

    add java script

--- app.js
+++ app.js
@@ -5,23 +5,36 @@ const button = document.querySelector("#btn");
 const bookList = document.querySelector("#book-list");
 const form = document.querySelector("form");
 
-const createTask = (title, author, date) => {
+const createTask = (title, author, date, delBtn) => {
   return `
-        <tr>
-          <td>${title}</td>
-          <td>${author}</td>
-          <td>${date}</td>
-        </tr>`
+  <tr>
+    <td>${title}</td>
+    <td>${author}</td>
+    <td>${date}</td>
+    <td class="del-btn">X</td>
+  </tr>`
 };
+
+
 
 button.addEventListener("click", (e) => {
   e.preventDefault();
   if (title.value != "" && author.value != "" && date.value != "") {
     bookList.innerHTML += createTask(title.value, author.value, date.value);
     form.reset();
-  
+    
     
   } else {
     alert("bos ");
+    form.reset();
+    
   }
+  
 });
+
+
+bookList.addEventListener("click", e => {
+  if (e.target.classList.contains("del-btn")) {
+    e.target.parentElement.remove();
+  }
+})
